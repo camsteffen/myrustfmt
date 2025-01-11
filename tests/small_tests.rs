2a1886359d73e84dbeeaf9cc5cc56a1e799240f6
Cameron Steffen <cam.steffen94@gmail.com>
2025-01-11 12:27:16 -0600

    prog

---
+++ tests/small_tests.rs
@@ -0,0 +1,62 @@
+#![feature(rustc_private)]
+
+mod test_lib;
+
+use crate::test_lib::{format_stmt_max_width_expected, stmt_breakpoint_test};
+use serde::Deserialize;
+use std::fs;
+use std::io::BufReader;
+use std::path::Path;
+use std::sync::LazyLock;
+
+macro_rules! breakpoint_tests {
+    ($($name:ident,)*) => {
+        $(
+        #[test]
+        fn $name() {
+            breakpoint_test_file(&Path::new("tests/small_tests").join(concat!(stringify!($name), ".yaml")));
+        }
+        )*
+    };
+}
+
+breakpoint_tests! {
+    array,
+    binop,
+    local,
+}
+
+fn breakpoint_test_file(test_source_path: &Path) {
+    println!("Running breakpoint tests in {}", test_source_path.display());
+    let file = fs::File::open(test_source_path).unwrap();
+    let reader = BufReader::new(file);
+    let tests: Vec<Test> = serde_yaml::from_reader(reader).unwrap();
+    for test in &tests {
+        breakpoint_test(test)
+    }
+}
+
+fn breakpoint_test(test: &Test) {
+    println!("Test: {}", &test.name);
+    match &test.kind {
+        TestKind::Breakpoint { before, after } => stmt_breakpoint_test(before, after),
+        TestKind::NoChange { formatted } => {
+            let formatted = formatted.trim();
+            format_stmt_max_width_expected(formatted, None, formatted)
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct Test {
+    name: String,
+    #[serde(flatten)]
+    kind: TestKind,
+}
+
+#[derive(Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "snake_case", tag = "type")]
+enum TestKind {
+    Breakpoint { before: String, after: String },
+    NoChange { formatted: String },
+}
---
+++ theory.md
@@ -0,0 +1,6 @@
+## Lists
+
+When the width of a list of things goes above a certain threshold, the list becomes more readable to read vertically.
+A list of one item should not be considered a list.
+A list of two items should not be considered a list.
+A dot chain is a list where in `a.b`, `a` is the first item in the list.
