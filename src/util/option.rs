dffaca6b64dc45bb7ca65f0970ed6e718093f134
Cameron Steffen <cam.steffen94@gmail.com>
2025-02-02 20:35:25 -0600

    prog

--- src/util/option.rs
+++
@@ -1,7 +0,0 @@
-pub fn merge_options<T>(a: Option<T>, b: Option<T>, merge: impl FnOnce(T, T) -> T) -> Option<T> {
-    match (a, b) {
-        (Some(a), Some(b)) => Some(merge(a, b)),
-        (Some(v), None) | (None, Some(v)) => Some(v),
-        (None, None) => None,
-    }
-}
