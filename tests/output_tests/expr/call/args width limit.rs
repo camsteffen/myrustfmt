b2c5d5a76641bba361190b552963f6e0d43beb1f
Cameron Steffen <cam.steffen94@gmail.com>
2025-06-15 21:09:16 -0500

    prog

--- tests/output_tests/expr/call/args width limit.rs
+++ tests/output_tests/expr/call/args width limit.rs
@@ -2,18 +2,35 @@
 // note: wrap arguments if there is more than one and they exceed 60 chars
 
 fn test() {
+    // single arg exceeds max width
     fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+    // multiple args at max width
     fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaaaaaaa);
+    // single arg, nested multiple args at max width
+    fun(fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaa));
+    // multiple args exceeds max width by 1
     fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+    // single arg, nested multiple args exceeds max width by 1
+    fun(fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaaa));
 }
 
 // :after:
 
 fn test() {
+    // single arg exceeds max width
     fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa);
+    // multiple args at max width
     fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaaaaaaa);
+    // single arg, nested multiple args at max width
+    fun(fun(aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaaaaaaa));
+    // multiple args exceeds max width by 1
     fun(
         aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
         aaaaaaaaaaaaaaaaaaaaaaaaaaaa,
     );
+    // single arg, nested multiple args exceeds max width by 1
+    fun(fun(
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa,
+        aaaaaaaaaaaaaaaaaaaaaaa,
+    ));
 }
