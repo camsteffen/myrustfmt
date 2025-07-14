615650a88a56aab8cfc38ff577286b3fcc403db6
Cameron Steffen <cam.steffen94@gmail.com>
2025-07-14 16:28:59 -0500

    prog

--- src/constraint_writer/checkpoint.rs
+++ src/constraint_writer/checkpoint.rs
@@ -4,13 +4,7 @@ use crate::num::VSize;
 use crate::util::cell_ext::CellExt;
 
 pub struct ConstraintWriterCheckpoint {
-    len: usize,
-    self_checkpoint: ConstraintWriterSelfCheckpoint,
-}
-
-// todo rename
-#[derive(Debug)]
-pub struct ConstraintWriterSelfCheckpoint {
+    buffer_len: usize,
     constraints_checkpoint: ConstraintsCheckpoint,
     line: VSize,
     last_line_start: usize,
@@ -19,22 +13,15 @@ pub struct ConstraintWriterSelfCheckpoint {
 
 impl ConstraintWriter {
     pub fn checkpoint(&self) -> ConstraintWriterCheckpoint {
-        ConstraintWriterCheckpoint {
-            self_checkpoint: self.self_checkpoint(),
-            len: self.len(),
-        }
-    }
-
-    pub fn self_checkpoint(&self) -> ConstraintWriterSelfCheckpoint {
         let Self {
             ref constraints,
             ref last_line_start,
             ref last_width_exceeded_line,
             ref line,
-            buffer: _,
-            errors: _,
+            ..
         } = *self;
-        ConstraintWriterSelfCheckpoint {
+        ConstraintWriterCheckpoint {
+            buffer_len: self.len(),
             constraints_checkpoint: constraints.checkpoint(),
             line: line.get(),
             last_line_start: last_line_start.get(),
@@ -44,20 +31,13 @@ impl ConstraintWriter {
 
     pub fn restore_checkpoint(&self, checkpoint: &ConstraintWriterCheckpoint) {
         let ConstraintWriterCheckpoint {
-            ref self_checkpoint,
-            len,
-        } = *checkpoint;
-        self.restore_self_checkpoint(self_checkpoint);
-        self.buffer.with_taken(|b| b.truncate(len));
-    }
-
-    pub fn restore_self_checkpoint(&self, checkpoint: &ConstraintWriterSelfCheckpoint) {
-        let ConstraintWriterSelfCheckpoint {
+            buffer_len,
             ref constraints_checkpoint,
             last_line_start,
             last_width_exceeded_line,
             line,
         } = *checkpoint;
+        self.buffer.with_taken(|b| b.truncate(buffer_len));
         self.constraints.restore_checkpoint(constraints_checkpoint);
         self.last_line_start.set(last_line_start);
         self.last_width_exceeded_line.set(last_width_exceeded_line);
